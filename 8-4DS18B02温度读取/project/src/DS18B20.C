15728da0bb89f84b660706939c06b47eb5a996e6
sakurajh <1853525104@qq.com>
2023-08-24 17:19:30 +0800

    8.24

---
+++ 8-4DS18B02温度读取/project/src/DS18B20.C
@@ -0,0 +1,32 @@
+#include<REG51.H>
+#include"OneWire.h"
+#include"LCD1602.h"
+
+#define DS18B20_SKIP_ROM    0xCC
+#define DS18B20_CONVERT_T    0x44
+#define DS18B20_READ_SCRATCHPAD   0xBE
+
+void DS18B20_Convert(void)
+{
+    OneWire_Init();
+    OneWire_SendByte(DS18B20_SKIP_ROM);
+    OneWire_SendByte(DS18B20_CONVERT_T);
+}
+
+
+float DS18B20_ReadT(void)
+{
+    unsigned char TLSB,TMSB;
+    int Temp;
+    float T;
+    OneWire_Init();
+    OneWire_SendByte(DS18B20_SKIP_ROM);
+    OneWire_SendByte(DS18B20_READ_SCRATCHPAD);
+    TLSB=OneWire_ReceiveByte();
+    TMSB=OneWire_ReceiveByte();
+    LCD_ShowBinNum(1,1,TMSB,8);
+    LCD_ShowBinNum(1,9,TLSB,8);
+    Temp=(TMSB<<8)|TLSB;
+    T=Temp/16.0;
+    return T;
+}
